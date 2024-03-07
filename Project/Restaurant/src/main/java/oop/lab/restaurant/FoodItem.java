3f4f1e136cd9c0369fc190200e0f876500adc110
Phan-Ngoc-Hanh-Nhi <155942839+nhiphan-4303@users.noreply.github.com>
2024-03-08 00:08:36 +0700

    v2

--- Project/Restaurant/src/main/java/oop/lab/restaurant/FoodItem.java
+++ Project/Restaurant/src/main/java/oop/lab/restaurant/FoodItem.java
@@ -9,5 +9,66 @@ package oop.lab.restaurant;
  * @author hanie
  */
 public class FoodItem {
-    
+
+    // thuộc tính
+    private String foodId;
+    private String foodName;
+    private double price;
+    private FoodType foodType;
+
+    // constructor
+    public FoodItem() {
+    }
+
+    public FoodItem(String foodId, String foodName, double price, FoodType foodType) {
+        this.foodId = foodId;
+        this.foodName = foodName;
+        this.price = price;
+        this.foodType = foodType;
+    }
+
+// getter setter 
+    public String getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(String foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public FoodType getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(FoodType foodType) {
+        this.foodType = foodType;
+    }
+
+// method tính giá từng món theo số lượng món ăn
+    public double calculatePrice(int quantity) {
+        return price * quantity;
+    }
+
+    // method trả về chuỗi kết quả
+    @Override
+    public String toString() {
+        return "Food ID: " + foodId + ", Name: " + foodName + ", Price: " + price;
+    }
+
 }
--- Project/Restaurant/src/main/java/oop/lab/restaurant/FoodManager.java
+++ Project/Restaurant/src/main/java/oop/lab/restaurant/FoodManager.java
@@ -4,10 +4,118 @@
  */
 package oop.lab.restaurant;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author hanie
  */
 public class FoodManager {
+
+    private ArrayList<FoodItem> foodList = new ArrayList<FoodItem>();
+    private ArrayList<FoodType> foodTypeList = new ArrayList<FoodType>();
+
+    // constructor
+    public FoodManager() {
+    }
+
+    //getter setter
+    public ArrayList<FoodItem> getFoodList() {
+        return foodList;
+    }
+
+    public void setFoodList(ArrayList<FoodItem> foodList) {
+        this.foodList = foodList;
+    }
+
+    public ArrayList<FoodType> getFoodTypeList() {
+        return foodTypeList;
+    }
+
+    public void setFoodTypeList(ArrayList<FoodType> foodTypeList) {
+        this.foodTypeList = foodTypeList;
+    }
+
+    //method check exist id of type
+    public boolean addFoodType(FoodType foT) {
+        for (FoodType ft : foodTypeList) {
+            if (ft.getFoodTypeId().equalsIgnoreCase(foT.getFoodTypeId())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //method check exist id of food
+    public boolean addFoodItem(FoodItem foI) {
+        for (FoodItem fi : foodList) {
+            if (fi.getFoodId().equalsIgnoreCase(foI.getFoodId())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //method edit type
+    public boolean editFoodType(FoodType foT) {
+        for (FoodType ft : foodTypeList) {
+            if (ft.getFoodTypeId().equalsIgnoreCase(foT.getFoodTypeId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //method edit food
+    public boolean editFoodItem(FoodItem foI) {
+        for (FoodItem fi : foodList) {
+            if (fi.getFoodId().equalsIgnoreCase(foI.getFoodId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //method delete type
+    public boolean deleteFoodType(FoodType foT) {
+        for (FoodType ft : foodTypeList) {
+            if (ft.getFoodTypeId().equalsIgnoreCase(foT.getFoodTypeId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //method delete food
+    public boolean deleteFoodItem(FoodItem foI) {
+        for (FoodItem fi : foodList) {
+            if (fi.getFoodId().equalsIgnoreCase(foI.getFoodId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //method find food by ID
+    public FoodItem findFoodItemById(String id) {
+        for (FoodItem fi : foodList) {
+            if (fi.getFoodId().equalsIgnoreCase(id)) {
+                return fi;
+            }
+        }
+        return null;
+    }
+
+    //method find food by name
+    public FoodItem findFoodItemByName(String name) {
+        for (FoodItem fi : foodList) {
+            if (fi.getFoodName().equalsIgnoreCase(name)) {
+                return fi;
+            }
+        }
+        return null;
+    }
     
+    
+
 }
