388b5422baeb58d42687eba862898e0529cb44e7
Phan-Ngoc-Hanh-Nhi <155942839+nhiphan-4303@users.noreply.github.com>
2024-03-24 16:44:22 +0700

    v1

---
+++ Final/21_22_02/src/main/java/oop/lab/_22_02/ShoesServicesTicket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package oop.lab._22_02;
+
+/**
+ *
+ * @author hanie
+ */
+public class ShoesServicesTicket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
