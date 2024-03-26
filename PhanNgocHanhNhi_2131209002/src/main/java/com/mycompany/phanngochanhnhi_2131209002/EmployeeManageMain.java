2014fa15c7954f1d63b1b4a81fede2f3c09f172d
Phan-Ngoc-Hanh-Nhi <155942839+nhiphan-4303@users.noreply.github.com>
2024-03-26 12:28:56 +0700

    v1

---
+++ PhanNgocHanhNhi_2131209002/src/main/java/com/mycompany/phanngochanhnhi_2131209002/EmployeeManageMain.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.phanngochanhnhi_2131209002;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class EmployeeManageMain {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        System.out.println("Enter the number of Employee: ");
+        int numberOfEmployee = sc.nextInt();
+        
+    }
+
+    private ArrayList<Employee> employeeList = new ArrayList<>();
+
+    public EmployeeManageMain() {
+    }
+
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(ArrayList<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public void inputList() {
+
+    }
+
+    public double taxOfAll(int numberOfEmployee) {
+        double taxOfAll = 0;
+        for (Employee e : employeeList) {
+            taxOfAll += e.calculateIncomeTax();
+        }
+        return taxOfAll;
+    }
+
+    public void inputList(int numberOfEmployee) {
+        for (int i = 0; i < numberOfEmployee; i++) {
+            Employee e = new Employee();
+            e.input();
+            employeeList.add(e);
+        }
+    }
+
+}
