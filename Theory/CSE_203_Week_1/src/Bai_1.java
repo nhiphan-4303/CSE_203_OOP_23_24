38d3141ebec2869d86ad9ec57645b9332112f870
Phan-Ngoc-Hanh-Nhi <155942839+nhiphan-4303@users.noreply.github.com>
2025-01-10 23:10:27 +0700

    update

---
+++ Theory/CSE_203_Week_1/src/Bai_1.java
@@ -0,0 +1,9 @@
+//Write a function to count how many prime number in an array
+public class Bai_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Theory/CSE_203_Week_1/src/Bai_2.java
@@ -0,0 +1,40 @@
+/*Write a Java program that receive the full name of a person and write function to:
+*Return the first name and last name except the middle name(Nguyen Van Chien -> Nguyen Chien)
+*Return the middle name 
+*Capital the full name
+*Uppercase all vowels and lowercase all consonants. (Ex: Nguyen Van Chien → ngUyEn vAn chIEn) 
+*/
+
+import java.util.*;
+
+public class Bai_2 {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String fullName = sc.nextLine();
+		String[] splitName = splitName(fullName);
+		output(splitName);
+		output_1(splitName);
+
+	}
+
+	public static void output_1(String[] n) {
+		for (int i = 1; i < n.length - 1; i++) {
+			System.out.print(n[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public static void output(String[] n) {
+		System.out.print(n[0] + " " + n[n.length - 1]);
+		System.out.println();
+
+	}
+
+	public static String[] splitName(String n) {
+		String[] splitName = n.split("\\s");
+		return splitName;
+
+	}
+
+}
